76a1f14fbb5c78122bce4f3bb8ae7dea08cbc95c
Devit951 <devit951@gmail.com>
2019-06-09 11:47:38 +0500

    Make code more cleaner

--- app/src/main/java/com/github/devit951/xoapp/HorizontalBoardInterceptor.kt
+++ app/src/main/java/com/github/devit951/xoapp/HorizontalBoardInterceptor.kt
@@ -3,19 +3,18 @@ package com.github.devit951.xoapp
 class HorizontalBoardInterceptor: BoardInterceptor {
     override fun interceptBoard(cells: Array<Array<Cell?>>, board: Board): Boolean{
         for (row in 0 until cells.size){
-            if (figuresInRowSame(cells[row])){
-                val winnerPlayer = cells[row][0]!!.player
-                board.onBoardAction.invoke(XOApp.context.getString(R.string.the_winner_is, winnerPlayer), board)
-                return true
+            var countOfSameFigureInRow = 0
+            for (column in 0 until cells.size){
+                if (cells[row][row] != null && cells[row][row] == cells[row][column]){
+                    countOfSameFigureInRow++
+                }
+                if (countOfSameFigureInRow == 3){
+                    val winnerPlayer = cells[row][row]
+                    board.onBoardAction.invoke(XOApp.context.getString(R.string.the_winner_is, winnerPlayer), board)
+                    return true
+                }
             }
         }
         return false
-    }
-
-    private fun figuresInRowSame(verticalCells: Array<Cell?>): Boolean{
-        if (verticalCells[0]?.player == null || verticalCells[1]?.player == null || verticalCells[2]?.player == null){
-            return false
-        }
-        return verticalCells[0]?.player?.figure == verticalCells[1]?.player?.figure && verticalCells[1]?.player?.figure == verticalCells[2]?.player?.figure
     }
 }
--- app/src/main/java/com/github/devit951/xoapp/VerticalBoardInterceptor.kt
+++ app/src/main/java/com/github/devit951/xoapp/VerticalBoardInterceptor.kt
@@ -3,14 +3,14 @@ package com.github.devit951.xoapp
 class VerticalBoardInterceptor: BoardInterceptor {
 
     override fun interceptBoard(cells: Array<Array<Cell?>>, board: Board): Boolean {
-        for (column in 0 until cells.size){
+        for (row in 0 until cells.size){
             var countOfSameFigureInColumn = 0
-            for (row in 0 until cells.size){
-                if (cells[column][column] != null && cells[column][column] == cells[row][column]){
+            for (column in 0 until cells.size){
+                if (cells[row][row] != null && cells[row][row] == cells[column][row]){
                     countOfSameFigureInColumn += 1
                 }
                 if (countOfSameFigureInColumn == 3){
-                    val winnerPlayer = cells[column][column]
+                    val winnerPlayer = cells[row][row]
                     board.onBoardAction.invoke(XOApp.context.getString(R.string.the_winner_is, winnerPlayer), board)
                     return true
                 }
