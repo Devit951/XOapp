2fbb720706bab5e49a9741cb31e2c7bf27c3d91a
Devit951 <devit951@gmail.com>
2019-06-09 11:52:07 +0500

    Removed duplicated code

--- app/src/main/java/com/github/devit951/xoapp/DiagonalBoardInterceptor.kt
+++ app/src/main/java/com/github/devit951/xoapp/DiagonalBoardInterceptor.kt
@@ -3,15 +3,18 @@ package com.github.devit951.xoapp
 class DiagonalBoardInterceptor: BoardInterceptor {
     override fun interceptBoard(cells: Array<Array<Cell?>>, board: Board): Boolean {
         if (cells[0][0] != null && cells[0][0] == cells[1][1] && cells[1][1] == cells[2][2]){
-            val winnerPlayer = cells[0][0]
-            board.onBoardAction.invoke(XOApp.context.getString(R.string.the_winner_is, winnerPlayer), board)
+            notifyWinnerPlayer(board, board.onBoardAction)
             return true
         }
         if (cells[2][0] != null && cells[2][0] == cells[1][1] && cells[1][1] == cells[0][2]){
-            val winnerPlayer = cells[2][0]
-            board.onBoardAction.invoke(XOApp.context.getString(R.string.the_winner_is, winnerPlayer), board)
+            notifyWinnerPlayer(board, board.onBoardAction)
             return true
         }
         return false
+    }
+
+    private fun notifyWinnerPlayer(board: Board, onBoardAction: (String, Board) -> Unit){
+        val winnerPlayer = board.cells[1][1]
+        onBoardAction.invoke(XOApp.context.getString(R.string.the_winner_is, winnerPlayer), board)
     }
 }
