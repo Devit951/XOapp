20f360c1424e8d89d3b9119acc11621ba7e57ac1
Devit951 <devit951@gmail.com>
2019-06-10 14:05:53 +0500

    Added XOActivity

---
+++ app/src/main/java/com/github/devit951/xoapp/XOActivity.kt
@@ -0,0 +1,19 @@
+package com.github.devit951.xoapp
+
+import android.app.Activity
+import android.os.Bundle
+import com.github.devit951.xoapp.xogame.BotAndHuman
+import com.github.devit951.xoapp.xogame.Figure
+import com.github.devit951.xoapp.xogame.Player
+import com.github.devit951.xoapp.xogame.XOView
+import com.github.devit951.xoapp.xogame.bot.EasyBot
+
+class XOActivity: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(XOView(this).apply {
+            startGame(listOf(BotAndHuman(Player(name = "SpaceX", figure = Figure.X), EasyBot(Figure.O))))
+        })
+    }
+}
