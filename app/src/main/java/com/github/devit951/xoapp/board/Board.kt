e653a2729c5db668e71c1fd4937007806c32f249
Devit951 <devit951@gmail.com>
2019-06-10 14:08:30 +0500

    Added array of buttons

--- app/src/main/java/com/github/devit951/xoapp/board/Board.kt
+++ app/src/main/java/com/github/devit951/xoapp/board/Board.kt
@@ -2,38 +2,50 @@ package com.github.devit951.xoapp.board
 
 import android.widget.Button
 import android.widget.GridLayout
+import com.github.devit951.xoapp.xogame.Moveable
 import com.github.devit951.xoapp.xogame.XOView
 
 class Board(private val xoView: XOView,
+            private val players: List<Moveable>,
             private val onBoardAction: (String) -> Unit){
 
     private var cells = Array<Array<Cell?>>(3) { arrayOfNulls(3) }
-    private var boardObserver = BoardObserver(cells, onBoardAction)
+    private var buttons = Array<Array<Button?>>(3) { arrayOfNulls(3) }
+    private var boardObserver = BoardObserver(cells, players, onBoardAction)
 
     init {
         invalidate()
     }
 
     fun invalidate(){
-        cells = Array<Array<Cell?>>(3) { arrayOfNulls(3) }
-        boardObserver = BoardObserver(cells, onBoardAction)
-        xoView.removeAllViews()
+        moveToDefaultState()
         xoView.apply {
             rowCount = 3
             columnCount = 3
-            for (row in 0 until 3){
-                for (column in 0 until 3){
-                    addView(Button(context).apply {
-                        setOnClickListener {
-                            boardObserver.move(BoardCoordinates(row, column), this)
-                        }
+            for (row in 0 until rowCount){
+                for (column in 0 until columnCount){
+                    Button(context).apply {
                         layoutParams = GridLayout.LayoutParams().apply {
                             columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f)
                             rowSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f)
                         }
-                    })
+                        buttons[row][column] = this
+                        addView(this)
+                        setOnClickListener {
+                            boardObserver.move(BoardCoordinates(row, column)){ boardCoordinates, currentPlayer ->
+                                buttons[boardCoordinates.row][boardCoordinates.column]!!.text = currentPlayer.figure.toString()
+                            }
+                        }
+                    }
                 }
             }
         }
+    }
+
+    private fun moveToDefaultState(){
+        cells = Array<Array<Cell?>>(3) { arrayOfNulls(3) }
+        boardObserver = BoardObserver(cells, players, onBoardAction)
+        buttons = Array<Array<Button?>>(3) { arrayOfNulls(3) }
+        xoView.removeAllViews()
     }
 }
