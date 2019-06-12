f2933e3e1184611638c63d3690f6b11ec5fee5ee
Devit951 <devit951@gmail.com>
2019-06-12 17:11:37 +0500

    Added OView

---
+++ app/src/main/java/com/github/devit951/xoapp/OView.kt
@@ -0,0 +1,19 @@
+package com.github.devit951.xoapp
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+import com.github.devit951.xoapp.xogame.shape.OShape
+
+class OView(context: Context,
+            attributeSet: AttributeSet? = null,
+            defAttr: Int = 0): View(context, attributeSet, defAttr) {
+
+    private val oShape = OShape()
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        oShape.drawShape(canvas)
+    }
+}
