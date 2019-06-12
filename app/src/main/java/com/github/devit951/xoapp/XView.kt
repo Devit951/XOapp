d82ff5cbd710156435e3b8a67d7e9afb325e6ea8
Devit951 <devit951@gmail.com>
2019-06-12 17:11:27 +0500

    Added XView

---
+++ app/src/main/java/com/github/devit951/xoapp/XView.kt
@@ -0,0 +1,19 @@
+package com.github.devit951.xoapp
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+import com.github.devit951.xoapp.xogame.shape.XShape
+
+class XView(context: Context,
+            attributeSet: AttributeSet? = null,
+            defAttr: Int = 0): View(context, attributeSet, defAttr){
+
+    private val xShape = XShape()
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        xShape.drawShape(canvas)
+    }
+}
