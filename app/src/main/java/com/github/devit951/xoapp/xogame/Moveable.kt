a3f5d15649657aa590b219aadf92b53f25af7610
Devit951 <devit951@gmail.com>
2019-06-13 15:29:22 +0500

    Make Moveable serializable

--- app/src/main/java/com/github/devit951/xoapp/xogame/Moveable.kt
+++ app/src/main/java/com/github/devit951/xoapp/xogame/Moveable.kt
@@ -2,7 +2,8 @@ package com.github.devit951.xoapp.xogame
 
 import com.github.devit951.xoapp.board.BoardCoordinates
 import com.github.devit951.xoapp.board.Cell
+import java.io.Serializable
 
-interface Moveable {
+interface Moveable: Serializable {
     fun move(boardCoordinates: BoardCoordinates, cells: Array<Array<Cell?>>, onMoved: (BoardCoordinates, Player) -> Unit)
 }
