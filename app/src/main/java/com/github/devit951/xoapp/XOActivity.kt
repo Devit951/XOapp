524c7af99e0986a2a4ea1f5db2d21bfafc976c9f
Devit951 <devit951@gmail.com>
2019-06-13 15:34:31 +0500

    XOActivity dynmically can receive players

--- app/src/main/java/com/github/devit951/xoapp/XOActivity.kt
+++ app/src/main/java/com/github/devit951/xoapp/XOActivity.kt
@@ -2,18 +2,21 @@ package com.github.devit951.xoapp
 
 import android.app.Activity
 import android.os.Bundle
-import com.github.devit951.xoapp.xogame.BotVsHuman
-import com.github.devit951.xoapp.xogame.Figure
-import com.github.devit951.xoapp.xogame.Player
-import com.github.devit951.xoapp.xogame.XOView
-import com.github.devit951.xoapp.xogame.bot.EasyBot
+import com.github.devit951.xoapp.xogame.*
 
 class XOActivity: Activity() {
+
+    companion object {
+        const val ARG_MOVEABLE_PLAYERS = "ARG_MOVEABLE_PLAYERS"
+    }
+
+    private val players: List<Moveable>
+        get() = intent.getSerializableExtra(ARG_MOVEABLE_PLAYERS) as List<Moveable>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(XOView(this).apply {
-            startGame(listOf(BotVsHuman(Player(name = "SpaceX", figure = Figure.X), EasyBot(Figure.O))))
+            startGame(players)
         })
     }
 }
