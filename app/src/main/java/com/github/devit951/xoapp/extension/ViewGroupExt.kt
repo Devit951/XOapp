20f434f7b1a924c3a898a1a9f7234c3e25089287
Devit951 <devit951@gmail.com>
2019-06-17 15:21:55 +0500

    Moved extensions to ext package

---
+++ app/src/main/java/com/github/devit951/xoapp/extension/ViewGroupExt.kt
@@ -0,0 +1,9 @@
+package com.github.devit951.xoapp.extension
+
+import android.view.ViewGroup
+
+val wrapContent: Int
+    get() = ViewGroup.LayoutParams.WRAP_CONTENT
+
+val matchParent: Int
+    get() = ViewGroup.LayoutParams.MATCH_PARENT
