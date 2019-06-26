3331be943376827d5f23b21988b5743bb8d2bb9b
Devit951 <devit951@gmail.com>
2019-06-26 16:15:11 +0500

    Make view fixed size 100dp

--- app/src/main/java/com/github/devit951/xoapp/xogame/OView.kt
+++ app/src/main/java/com/github/devit951/xoapp/xogame/OView.kt
@@ -1,9 +1,12 @@
 package com.github.devit951.xoapp.xogame
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
+import com.github.devit951.xoapp.extension.dp
 import com.github.devit951.xoapp.xogame.shape.OShape
 
 class OView(context: Context,
@@ -15,5 +18,10 @@ class OView(context: Context,
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         oShape.drawShape(canvas)
+    }
+
+    @SuppressLint("SwitchIntDef")
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        setMeasuredDimension(100.dp, 100.dp)
     }
 }
--- app/src/main/java/com/github/devit951/xoapp/xogame/XView.kt
+++ app/src/main/java/com/github/devit951/xoapp/xogame/XView.kt
@@ -1,9 +1,11 @@
 package com.github.devit951.xoapp.xogame
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.util.AttributeSet
 import android.view.View
+import com.github.devit951.xoapp.extension.dp
 import com.github.devit951.xoapp.xogame.shape.XShape
 
 class XView(context: Context,
@@ -15,5 +17,10 @@ class XView(context: Context,
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         xShape.drawShape(canvas)
+    }
+
+    @SuppressLint("SwitchIntDef")
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        setMeasuredDimension(100.dp, 100.dp)
     }
 }
