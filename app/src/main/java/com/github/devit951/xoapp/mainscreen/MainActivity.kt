50ffa7f5ae76b8d05f9059d2ce847555b373052e
Devit951 <devit951@gmail.com>
2019-06-28 17:30:51 +0500

    Added flag keep screen on

--- app/src/main/java/com/github/devit951/xoapp/mainscreen/MainActivity.kt
+++ app/src/main/java/com/github/devit951/xoapp/mainscreen/MainActivity.kt
@@ -2,11 +2,13 @@ package com.github.devit951.xoapp.mainscreen
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         setContentView(MainView(this))
     }
 }
