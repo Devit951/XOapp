47a9ca778f7c0242173199f76f3233f4c7770833
Devit951 <devit951@gmail.com>
2019-07-03 15:29:52 +0500

    Added randomly translation views

--- app/src/main/java/com/github/devit951/xoapp/xogame/RandomTranslationView.kt
+++ app/src/main/java/com/github/devit951/xoapp/xogame/RandomTranslationView.kt
@@ -4,64 +4,91 @@ import android.graphics.Canvas
 import android.util.Log
 import android.view.View
 import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.math.abs
+import kotlin.random.Random
 
 class RandomTranslationView(val origin: View) {
 
     companion object {
-        private const val TRANSLATE_SPEED = 10
+        private val TAG = RandomTranslationView::class.java.simpleName
     }
 
-    private val random = Random()
-
-    private val road: Queue<Coordinates> = LinkedList()
-    private var currentDirection = Direction.LEFT_TO_RIGHT
-    private var yPoint = 0
+    private val road: Queue<Coordinate> = LinkedList()
+    private var currentDirection = Direction.values().get(0)
+    private var lastCoordinate: Coordinate? = null
 
     fun randomlyTranslate(canvas: Canvas){
-        yPoint = random.nextInt(canvas.height - origin.width)
+        val randomY = Random.nextDouble(canvas.height - origin.width.toDouble()).toFloat()
         if (road.isEmpty()){
             when(currentDirection){
                 Direction.LEFT_TO_RIGHT -> {
-                    for (i in 0..canvas.width){
-                        val calculatedX = i * TRANSLATE_SPEED
-                        if (calculatedX >= canvas.width - origin.width){
-                            break
-                        }
-                        road.add(Coordinates(calculatedX, yPoint))
+                    if (lastCoordinate == null){
+                        lastCoordinate = Coordinate(0f, randomY)
                     }
+                    val secondCoordinate = Coordinate(canvas.width.toFloat(), randomY)
+                    road.addAll(lastCoordinate!!.horizontalDistanceLeftToRight(secondCoordinate))
+                    lastCoordinate = secondCoordinate
                 }
                 Direction.RIGHT_TO_LEFT -> {
-                    for (i in canvas.width downTo 0){
-                        val calculatedX = i - ((canvas.width - i) * TRANSLATE_SPEED) - origin.width
-                        if (calculatedX <= 0){
-                            break
-                        }
-                        road.add(Coordinates(calculatedX, yPoint))
+                    if (lastCoordinate == null){
+                        lastCoordinate = Coordinate(canvas.width.toFloat(), randomY)
                     }
+                    val secondCoordinate = Coordinate(0f, randomY)
+                    road.addAll(lastCoordinate!!.horizontalDistanceRightToLeft(secondCoordinate))
+                    lastCoordinate = secondCoordinate
                 }
             }
 
         } else {
             val coordinate = road.remove()
-            val originSize = coordinate.x + origin.width
+            val xOriginSize = coordinate.x + origin.width
 
-            origin.y = coordinate.y.toFloat()
-            origin.x = coordinate.x.toFloat()
+            origin.x = coordinate.x
+            origin.y = coordinate.y
 
             when{
-                originSize >= (canvas.width - origin.width) -> {
+                xOriginSize >= (canvas.width - origin.width) -> {
                     currentDirection = Direction.RIGHT_TO_LEFT
                 }
-                originSize <= canvas.width -> {
+                xOriginSize <= canvas.width -> {
                     currentDirection = Direction.LEFT_TO_RIGHT
                 }
             }
-            Log.d("test333", "coordinate x = ${coordinate.x},  direction = $currentDirection")
+            Log.d(TAG, "coordinate $coordinate,  direction = $currentDirection")
         }
     }
 
 
-    private data class Coordinates(val x: Int, val y: Int)
+    private inner class Coordinate(val x: Float, val y: Float){
+        fun horizontalDistanceLeftToRight(anotherCoordinate: Coordinate): List<Coordinate>{
+            val coordinates = ArrayList<Coordinate>()
+            val translationSpeed = Random.nextDouble(10.0, 30.0).toFloat()
+            val yDiff = abs(y - anotherCoordinate.y) / anotherCoordinate.x
+            for (i in 0..anotherCoordinate.x.toInt()){
+                val calculatedX = x + i * translationSpeed
+                val calculatedY = if (anotherCoordinate.y > y) y + yDiff * i * translationSpeed else y - yDiff * i * translationSpeed
+                if (calculatedX >= anotherCoordinate.x - origin.width) break
+                coordinates.add(Coordinate(calculatedX, calculatedY))
+            }
+            return coordinates
+        }
+
+        fun horizontalDistanceRightToLeft(anotherCoordinate: Coordinate): List<Coordinate>{
+            val coordinates = ArrayList<Coordinate>()
+            val translationSpeed = Random.nextDouble(10.0, 30.0).toFloat()
+            val yDiff = abs(y - anotherCoordinate.y) / x
+            for (i in x.toInt() downTo 0){
+                val calculatedX = x - ((x - i) * translationSpeed) - origin.width
+                val calculatedY = if (anotherCoordinate.y > y) y + (yDiff *(x - i) * translationSpeed) - (origin.height - translationSpeed) else y - (yDiff *(x - i) * translationSpeed) - (origin.height - translationSpeed)
+                if (calculatedX <= 0) break
+                coordinates.add(Coordinate(calculatedX, calculatedY))
+            }
+            return coordinates
+        }
+
+        override fun toString() = "Coordinate(x=$x, y=$y)"
+    }
     private enum class Direction{
         LEFT_TO_RIGHT, RIGHT_TO_LEFT
     }
